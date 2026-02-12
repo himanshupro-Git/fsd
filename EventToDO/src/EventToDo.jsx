daefd6deaaad9f9986e1c623b315d97b558f7fe6
himanshupro-Git <himanshu.learner.pro@gmail.com>
2026-02-12 15:56:21 +0530

    new button

--- EventToDO/src/EventToDo.jsx
+++ EventToDO/src/EventToDo.jsx
@@ -1,12 +1,17 @@
 export default function EventToDo()
 {   function handleClick(name){
-    alert(`The button is clicked by ${name}`)
+    alert(`The button is clicked by ${name}`);
+
+    function handleBgColor(){
+        
+    }
 }
   return(
     <div>
       <h1>Event Handling</h1>
       {/* <button onClick={handleClick}>Click Me</button> */}
       <button onClick={()=>handleClick("me")}>Click Me</button>
+      <button onClick={handleBgColor}>change color</button>
     </div>
   )
 }
