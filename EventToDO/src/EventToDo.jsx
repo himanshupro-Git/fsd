36b930844581d0105dc2b14f1702b7515411f496
himanshupro-Git <himanshu.learner.pro@gmail.com>
2026-02-12 15:47:00 +0530

    using prop in function for eventListner

--- EventToDO/src/EventToDo.jsx
+++ EventToDO/src/EventToDo.jsx
@@ -1,11 +1,12 @@
 export default function EventToDo()
-{   function handleClick(){
-    alert("You just clicked the Button!!");
+{   function handleClick(name){
+    alert(`The button is clicked by ${name}`)
 }
   return(
     <div>
       <h1>Event Handling</h1>
-      <button onClick={handleClick}>Click Me</button>
+      {/* <button onClick={handleClick}>Click Me</button> */}
+      <button onClick={()=>handleClick("me")}>Click Me</button>
     </div>
   )
 }
